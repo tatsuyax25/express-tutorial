97e80013d15c3dab1be6300128774fdd043b743a
Miguel Urena <urena_miguel82@outlook.com>
2024-05-18 11:42:58 -0700

    Add home and boilerplate

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 // Mount routes (app.use)
 // Define a "root" route directly on app
 // Tomorrow, we'll use best practice routing
-app.get('/', function (req, res) {
-  res.send('<h1>Hello Express</h1>');
+app.get('/home', function (req, res) {
+  res.send('<h1>Home Page</h1>');
 });
 
 // Tell the app to listen on port 3000
